e2449bad92626d4d92e6fa63325820f64c436246
Kourosh Gharibdoust <kourosh.gd351@gmail.com>
2025-04-07 18:23:20 +0300

    Added kourosh project

---
+++ Kourosh-FrontClass/js/project/project.js
@@ -0,0 +1,24 @@
+var usernum = 213;
+
+
+if (usernum % 2 == 0) {
+    console.log(`${usernum}, is an even number.`);
+} else {
+    console.log(`${usernum}, is an odd number.`);
+}
+
+var usernum = 456;
+
+if (usernum % 2 == 0) {
+    console.log(`${usernum}, is an even number.`);
+} else {
+    console.log(`${usernum}, is an odd number.`);
+}
+
+var usernum = 312;
+
+if (usernum % 2 == 0) {
+    console.log(`${usernum}, is an even number.`);
+} else {
+    console.log(`${usernum}, is an odd number.`);
+}
